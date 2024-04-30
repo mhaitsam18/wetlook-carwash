7f0bcc2ac95b103184193f43d39c5f901135b5e5
mhaitsam18 <haitsam03@gmail.com>
2024-05-01 00:45:48 +0700

    Middleware

---
+++ app/Http/Middleware/IsMember.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsMember
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()->role == 'member') {
+            return $next($request);
+        }
+        abort(403);
+        // return response()->view('errors.index', [
+        //     'title' => 'Akses ditolak',
+        //     'message' => 'Anda dilarang mengakses halaman ini',
+        //     'code' => '403',
+        // ], 403);
+    }
+}
