bb293a4080494c7219474790033d44d685472a34
mhaitsam18 <haitsam03@gmail.com>
2024-05-01 22:43:40 +0700

    Templating blade component tahap 1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    function index(): View
+    {
+        return view('home', [
+            'title' => 'Beranda'
+        ]);
+    }
+}
