7155baff83cd6ef6143766c79ac8c87cba7aed94
mhaitsam18 <haitsam03@gmail.com>
2024-05-08 11:38:57 +0700

    Tambah menu pada Navbar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,15 @@
-@props(['active' => false])
-<li>
-    <a {{ $attributes }} class="{{ $active ? 'active' : '' }} main-menu-link" aria-current="{{ $active ? 'page' : false }} ">{{ $slot }}</a>
+@props(['active' => false, 'dropdown' => false, 'submenus' => false])
+<li class="{{ $dropdown }}">
+    <a {{ $attributes }} class="{{ $active ? 'active' : '' }} main-menu-link"
+        aria-current="{{ $active ? 'page' : false }} ">{{ $slot }}
+        {!! $dropdown ? '<i class="fa fa-angle-down"></i>' : '' !!}</a>
+    @if ($dropdown)
+        <ul class="sub-menu">
+            @if ($slot == 'Pesanan Saya')
+                @foreach ($submenus as $submenu)
+                    <li><a href="{{ $submenu['href'] }}">{{ $submenu['slot'] }}</a></li>
+                @endforeach
+            @endif
+        </ul>
+    @endif
 </li>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,25 @@
 @props(['wide'])
+@php
+    $submenu = [
+        'pesanan-saya' => [
+            [
+                'href' => 'bookings',
+                'slot' => 'Bookingan Saya',
+            ],
+            [
+                'href' => 'history',
+                'slot' => 'Histori Pemesanan',
+            ],
+        ],
+    ];
+@endphp
 <nav>
     <ul>
         <x-nav-link href="/" :active="request()->is('/')">Beranda</x-nav-link>
         @can('member')
             <x-nav-link href="/booking" :active="request()->is('booking')">Booking</x-nav-link>
+            <x-nav-link href="#" :active="request()->is('orders/*')" :dropdown="'has-dropdown'" :submenus="$submenu['pesanan-saya']">Pesanan Saya</x-nav-link>
+            <x-nav-link href="/my-profile" :active="request()->is('my-profile')">Profil Saya</x-nav-link>
         @endcan
         @guest
             <x-nav-link href="/register" :active="request()->is('register')">Registrasi</x-nav-link>
