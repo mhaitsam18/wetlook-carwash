58152647846102f457b5c342805ddd9c78e85810
mhaitsam18 <haitsam03@gmail.com>
2024-05-09 00:05:03 +0700

    Alert

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <div class="alert alert-danger mb-3 mx-auto flash-data" style="width: 50%;" role="alert"
+        data-{{ $type }}="{{ $message }}">
+        {{ $message }}
+    </div>
+</div>
