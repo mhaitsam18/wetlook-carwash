e6d109a812007f32b61a40a1a3b40081c67bafd5
mhaitsam18 <haitsam03@gmail.com>
2024-05-14 11:48:56 +0700

    Make Controller and Route for admin role

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\AdminAdminController;
+use App\Http\Controllers\AdminBookingController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminMemberController;
+use App\Http\Controllers\AdminOrderController;
+use App\Http\Controllers\AdminOrderDetailController;
+use App\Http\Controllers\AdminPackageController;
+use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\AdminRoomController;
+use App\Http\Controllers\AdminVehicleController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MemberBookingController;
@@ -32,8 +42,25 @@ Route::middleware('auth')->group(function () {
         Route::put('/my-profile/{member}', [MemberController::class, 'update'])->name('update.member');
         Route::put('/change-password/{user}', [MemberController::class, 'updatePassword'])->name('update.password');
 
-
         Route::get('/bookingan-saya', [MemberBookingController::class, 'booking'])->name('booking');
+    });
+    Route::middleware('admin')->group(function () {
+        Route::prefix('admin')->group(function () {
+            Route::get('/index', [AdminController::class, 'index'])->name('admin.index');
+            Route::get('/my-profile', [AdminController::class, 'index'])->name('admin.my-profile');
+            Route::put('/my-profile/{admin}', [AdminController::class, 'update'])->name('update.admin');
+            Route::put('/change-password/{user}', [AdminController::class, 'updatePassword'])->name('update.password-admin');
+
+            Route::resource('admin', AdminAdminController::class);
+            Route::resource('member', AdminMemberController::class);
+            Route::resource('vehicle', AdminVehicleController::class);
+            Route::resource('package', AdminPackageController::class);
+            Route::resource('product', AdminProductController::class);
+            Route::resource('room', AdminRoomController::class);
+            Route::resource('booking', AdminBookingController::class);
+            Route::resource('order', AdminOrderController::class);
+            Route::resource('order.detail', AdminOrderDetailController::class);
+        });
     });
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout.post');
