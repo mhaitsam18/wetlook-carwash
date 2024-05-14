04938479b94ac52873a0e937d4cf321e1368fef0
mhaitsam18 <haitsam03@gmail.com>
2024-05-15 00:29:07 +0700

    Update my-profile.blade.php

--- resources/views/admin/my-profile.blade.php
+++ resources/views/admin/my-profile.blade.php
@@ -12,15 +12,16 @@
         <div class="card">
             <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
 
-                <img src="/assets-niceadmin/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-                <h2>Kevin Anderson</h2>
-                <h3>Web Designer</h3>
-                <div class="social-links mt-2">
+                <img src="{{ asset('storage/' . auth()->user()->photo) }}" alt="Profile" class="rounded-circle w-20"
+                    style="max-width: 150px;">
+                <h2>{{ auth()->user()->name }}</h2>
+                <h3>{{ auth()->user()->role }}</h3>
+                {{-- <div class="social-links mt-2">
                     <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
                     <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
                     <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
                     <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                </div>
+                </div> --}}
             </div>
         </div>
 
